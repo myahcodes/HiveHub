329212ca1ac281b1ae3d9f394c0d121df0470e5b
Alana Henry <mir.alana2294@gmail.com>
2025-10-20 19:19:10 -0500

    Moved CSS to login.css

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,22 +6,12 @@ const footer = document.querySelector(".HH-footer");
 /*Create form*/
 
 const form = document.createElement("form");
-
+form.className = "login_form";
 /*Username field*/
 
 const txt_username = document.createElement("input");
 txt_username.type = "text";
 txt_username.placeholder = "Username, email";
-
-txt_username.style.display = "block";
-
-txt_username.style.margin = "auto";
-
-txt_username.color = "#ffb84d";
-txt_username.placeholder.color = "#ffb84d";
-
-txt_username.style.borderStyle = "solid";
-txt_username.style.borderColor = "#ffb84d";
 
 /*Password field*/
 
@@ -29,37 +19,18 @@ const txt_password = document.createElement("input");
 txt_password.type = "password";
 txt_password.placeholder = "Password";
 
-txt_password.style.display = "block";
-
-txt_password.style.margin = "auto";
-
-txt_password.style.borderStyle = "solid";
-txt_password.style.borderColor = "#ffb84d";
-
-/*CSS and creation of Login Button*/
+/*Creation of Login Button*/
 
 const btn_Login = document.createElement("button");
 btn_Login.textContent = "Login";
+btn_Login.id = "login_button";
 btn_Login.type = "submit";
 
-btn_Login.style.color = "black";
-
-btn_Login.style.display = "block";
-
-btn_Login.style.margin = "auto";
-btn_Login.style.padding = "0.4cm 2.5cm 0.4cm 2.5cm";
-
-/*CSS and creation of Signup Button*/
+/*Creation of Signup Button*/
 
 const btn_signup = document.createElement("button");
 btn_signup.textContent = "Sign Up";
-
-btn_signup.style.color = "#ffb84d";
-
-btn_signup.style.display = "block";
-
-btn_signup.style.margin = "auto";
-btn_signup.style.padding = "0.4cm 2.5cm 0.4cm 2.5cm";
+btn_signup.id = "signup_button";
 
 /*Place buttons in the form*/
 
