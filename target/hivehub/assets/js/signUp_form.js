398065ae1d825700304aba2d4437c9c4e87bfaa1
Alana <165056608+Kittykatz911@users.noreply.github.com>
2025-11-04 01:57:22 +0000

    Updated WAR

--- target/hivehub/assets/js/signUp_form.js
+++ target/hivehub/assets/js/signUp_form.js
@@ -25,12 +25,14 @@ txt_name.placeholder = "Name";
 const txt_username = document.createElement("input");
 txt_username.type = "text";
 txt_username.placeholder = "Username";
+txt_username.required = true;
 
 /*Password field*/
 
 const txt_password = document.createElement("input");
 txt_password.type = "password";
 txt_password.placeholder = "Password";
+txt_password.required = true;
 
 /*Creation of Login Button*/
 
@@ -57,9 +59,26 @@ form.appendChild(txt_email);
 form.appendChild(txt_name);
 form.appendChild(txt_username);
 form.appendChild(txt_password);
-form.appendChild(btn_Login);
 form.appendChild(btn_signup);
+form.appendChild(btn_Login);
+
 
 /*Place form in HH-contents block*/
 
 container.appendChild(form);
+
+/*Move to the login page on clicking  SignUp button*/
+btn_signup.addEventListener("click", function(event) {
+    // Check form validity first
+    if (!form.checkValidity()) {
+        // If form is not valid, error prompt
+        return;
+    }
+    
+    form.submit(); // Submit the form
+    window.location.href = "Login.html"; // Redirects to login page
+});
+
+btn_Login.addEventListener("click", function(event) {
+    window.location.href = "Home.html";
+});
