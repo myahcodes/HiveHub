c16c72555886d65bdfc4f8d77f8e419d592094d7
Alana <165056608+Kittykatz911@users.noreply.github.com>
2025-11-06 16:14:18 +0000

    Updated js

--- src/main/webapp/assets/js/profile.js
+++ src/main/webapp/assets/js/profile.js
@@ -1,14 +1,33 @@
+
 /*Reference containers*/
 const pf_container = document.querySelector(".profile-container");
 const buzz_container = document.querySelector(".buzz-container");
 const footer = document.querySelector(".HH-footer");
 
 /*Create profile picture element*/
+const pfp_outline = document.createElement('img');
+pfp_outline.src = 'assets/icons/combBlank.svg';
+pfp_outline.width = 150;
+pfp_outline.float = "center";
+pfp_outline.onerror = function () { console.log("Error loading profile picture border."); };
+
+pfp_outline.addEventListener('mouseover', () => {
+    pfp_outline.classList.add('shake');
+
+    pfp_outline.addEventListener('animationend', () =>
+        pfp_outline.classList.remove('shake'),
+        { once: true }
+    );
+});
 
 const pfp_Icon = document.createElement('img');
 pfp_Icon.src = 'assets/icons/defaultPfp.svg';
-pfp_Icon.width = 64;
+pfp_Icon.width = 150;
+pfp_Icon.float = "center";
 pfp_Icon.onerror = function () { console.log("Error loading profile picture."); };
+
+/*Create settings icon*/
+const settings_icon = document.createElement('img');
 
 /*Create My Buzz elements*/
 
@@ -25,6 +44,7 @@ buzz.textContent = "Buzz";
 buzz.style.padding = "4px 25px";
 
 /*Place elements*/
+pf_container.appendChild(pfp_outline);
 pf_container.appendChild(pfp_Icon);
 
 buzz_container.appendChild(new_buzz);
