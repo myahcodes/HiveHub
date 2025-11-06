f09d0bcf9c7a9388e52632ef557b34b87b4b574a
Alana Henry <mir.alana2294@gmail.com>
2025-11-05 19:10:59 -0600

    Created menu icon js file and starting code

---
+++ src/main/webapp/assets/js/Icons.js
@@ -0,0 +1,7 @@
+/*Reference declarations*/
+
+/*Create icons*/
+const home_icon = document.createElement('img');
+const search_icon = document.createElement('img');
+const bb_icon = document.createElement('img');
+/*Place icons*/
