e83d3e79b955d323d91c1f95788b49dd06853c2f
Alana <165056608+Kittykatz911@users.noreply.github.com>
2025-11-06 16:20:59 +0000

    Updated WAR

---
+++ target/hivehub/assets/js/Icons.js
@@ -0,0 +1,11 @@
+/*Reference declarations*/
+const container = document.querySelector('body');
+/*Create icons*/
+const home_icon = document.createElement('img');
+home_icon.width = 40;
+home_icon.style.zIndex = '3';
+home_icon.style.float = 'top left';
+const search_icon = document.createElement('img');
+const bb_icon = document.createElement('img');
+/*Place icons*/
+container.appendChild(home_icon);
