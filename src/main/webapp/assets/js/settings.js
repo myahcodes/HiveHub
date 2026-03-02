642eb936670f05a410a3013a417547d52005c059
Alana Henry <mir.alana2294@gmail.com>
2026-03-01 18:40:40 -0600

    changed special effects and updated for new option class

--- src/main/webapp/assets/js/settings.js
+++ src/main/webapp/assets/js/settings.js
@@ -1,21 +1,16 @@
 const hover_control = new Audio("/webapp/assets/audio/47313572-pop-on-269286.mp3");
 const click_audio = new Audio("/webapp/assets/audio/classic-click.mp3");
 
-const option_container = document.querySelectorAll(".options *");
+const options_effects = document.querySelectorAll(".option");
 
-option_container.forEach(element => {
-    element.addEventListener("mouseenter", () => {
+options_effects.forEach(option => {
+    option.addEventListener("mouseenter", () => {
         hover_control.currentTime = 0;
         hover_control.play();
     });
 
-    element.addEventListener("click", () => {
+    option.addEventListener("click", () => {
         click_audio.currentTime = 0;
         click_audio.play();
     });
 });
-
-
-
-
-
